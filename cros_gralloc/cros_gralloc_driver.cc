8dc3668acf93a587bffe9b315d71285618981c2f
Mauro Rossi <issor.oruam@gmail.com>
2020-01-25 21:34:29 +0100

    Add framebuffer HAL implementation based on drm_framebuffer (v3)
    (v1) original lambdadroid implementation
    
    (v2) changes in drv.h due to following commits:
         48b4c08 ("minigbm: fix flags to align with GBM")
         6e6dc49 ("minigbm: virtio: restrict formats supported without 3D")
    
    (v3) changes in drv.h due to aosp master branch minigbm implementation
    
    NOTE: #define BO_USE_FRAMEBUFFER (1ull << 19) as the fist available bit

--- cros_gralloc/cros_gralloc_driver.cc
+++ cros_gralloc/cros_gralloc_driver.cc
@@ -26,6 +26,10 @@ cros_gralloc_driver::~cros_gralloc_driver()
 	}
 }
 
+int cros_gralloc_driver::get_fd() const {
+	return drv_get_fd(drv_);
+}
+
 int32_t cros_gralloc_driver::init()
 {
 	/*
@@ -69,6 +73,18 @@ int32_t cros_gralloc_driver::init()
 			if (drv_)
 				return 0;
 		}
+	}
+
+	return -ENODEV;
+}
+
+int cros_gralloc_driver::init_master()
+{
+	int fd = open(DRM_DIR_NAME "/card0", O_RDWR, 0);
+	if (fd >= 0) {
+		drv_ = drv_create(fd);
+		if (drv_)
+			return 0;
 	}
 
 	return -ENODEV;
