780cf39af8cafd60971b45ce72b379e8e0504740
Mauro Rossi <issor.oruam@gmail.com>
2020-12-22 14:32:53 +0100

    generic_dri: order scanout_render_formats[] as per other backends
    Just a reordering to keep the existing code style

--- generic_dri_driver.c
+++ generic_dri_driver.c
@@ -29,9 +29,9 @@ static uint32_t generic_dri_resolve_format(struct driver *drv, uint32_t format, 
 	}
 }
 
-static const uint32_t scanout_render_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_XRGB8888,
-						   DRM_FORMAT_ABGR8888, DRM_FORMAT_XBGR8888,
-						   DRM_FORMAT_RGB565 };
+static const uint32_t scanout_render_formats[] = { DRM_FORMAT_ABGR8888, DRM_FORMAT_ARGB8888,
+						   DRM_FORMAT_RGB565,     DRM_FORMAT_XBGR8888,
+						   DRM_FORMAT_XRGB8888 };
 
 static const uint32_t texture_only_formats[] = { DRM_FORMAT_NV12, DRM_FORMAT_NV21,
 						 DRM_FORMAT_YVU420, DRM_FORMAT_YVU420_ANDROID };
