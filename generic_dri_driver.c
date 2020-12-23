lineage-rpi/android_external_minigbm

commit ea2cf12a89ebd341e31abdaa61e0a4521ffaae48

Add generic DRI backend
mesa3d project support graphic buffer allocation via DRI API for
various set of hardware, including systems with splitted GPU
and Display controller.

Use it as fallback backend in case no other backends were found in
the supported list.

Tested with Android on sun4i-drm and lima GPU (Pinephone).

Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
Change-Id: I9d6ed0bdf3ef255c3c4f9be5385a7b87b571a232

generic_dri_driver.c
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <string.h>
+#include <xf86drm.h>
+
+#include "dri.h"
+#include "drv_priv.h"
+#include "helpers.h"
+#include "util.h"
+#include <errno.h>
+
+static uint32_t generic_dri_resolve_format(struct driver *drv, uint32_t format, uint64_t use_flags)
+{
+	switch (format) {
+	case DRM_FORMAT_FLEX_IMPLEMENTATION_DEFINED:
+		/* Camera subsystem requires NV12. */
+		if (use_flags & (BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE))
+			return DRM_FORMAT_NV12;
+		/*HACK: See b/28671744 */
+		return DRM_FORMAT_XBGR8888;
+	case DRM_FORMAT_FLEX_YCbCr_420_888:
+		return DRM_FORMAT_NV12;
+	default:
+		return format;
+	}
+}
+
+static const uint32_t scanout_render_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_XRGB8888,
+						   DRM_FORMAT_ABGR8888, DRM_FORMAT_XBGR8888,
+						   DRM_FORMAT_BGR888,	DRM_FORMAT_BGR565 };
+
+static const uint32_t texture_only_formats[] = { DRM_FORMAT_NV12, DRM_FORMAT_NV21,
+						 DRM_FORMAT_YVU420, DRM_FORMAT_YVU420_ANDROID };
+
+static int replace_char(char *str, char orig, char rep)
+{
+	char *ix = str;
+	int n = 0;
+	while ((ix = strchr(ix, orig)) != NULL) {
+		*ix++ = rep;
+		n++;
+	}
+	return n;
+}
+
+static int generic_dri_driver_init(struct driver *drv)
+{
+	struct dri_driver *dri;
+
+	drmVersionPtr drm_version;
+	drm_version = drmGetVersion(drv->fd);
+
+	if (!drm_version)
+		return -ENOMEM;
+
+	dri = calloc(1, sizeof(struct dri_driver));
+	if (!dri)
+		return -ENOMEM;
+
+	drv->priv = dri;
+
+	char dri_pathname[64];
+
+	sprintf(dri_pathname, STRINGIZE(DRI_DRIVER_DIR) "/%s_dri.so", drm_version->name);
+
+	char drv_suffix[32];
+	strcpy(drv_suffix, drm_version->name);
+	drmFreeVersion(drm_version);
+
+	replace_char(drv_suffix, '-', '_');
+
+	if (dri_init(drv, dri_pathname, drv_suffix)) {
+		drv_log("dri_init failed for (%s) , (%s)", dri_pathname, drv_suffix);
+		free(dri);
+		drv->priv = NULL;
+		return -ENODEV;
+	}
+
+	drv_add_combinations(drv, scanout_render_formats, ARRAY_SIZE(scanout_render_formats),
+			     &LINEAR_METADATA, BO_USE_RENDER_MASK | BO_USE_SCANOUT);
+
+	drv_add_combinations(drv, texture_only_formats, ARRAY_SIZE(texture_only_formats),
+			     &LINEAR_METADATA, BO_USE_TEXTURE_MASK);
+
+	drv_modify_combination(drv, DRM_FORMAT_NV12, &LINEAR_METADATA,
+			       BO_USE_HW_VIDEO_ENCODER | BO_USE_HW_VIDEO_DECODER |
+				   BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE);
+	drv_modify_combination(drv, DRM_FORMAT_NV21, &LINEAR_METADATA, BO_USE_HW_VIDEO_ENCODER);
+
+	return drv_modify_linear_combinations(drv);
+}
+
+const struct backend backend_dri_generic = {
+	.name = "dri_generic",
+	.init = generic_dri_driver_init,
+	.bo_create = dri_bo_create,
+	.bo_destroy = dri_bo_destroy,
+	.bo_import = dri_bo_import,
+	.bo_map = dri_bo_map,
+	.bo_unmap = dri_bo_unmap,
+	.bo_get_plane_fd = dri_bo_get_plane_fd,
+	.resolve_format = generic_dri_resolve_format,
+};

Android.bp
@@ -6,9 +6,11 @@ cc_defaults {
 
     srcs: [
         "amdgpu.c",
+        "dri.c",
         "drv.c",
         "dumb_driver.c",
         "exynos.c",
+        "generic_dri_driver.c",
         "helpers_array.c",
         "helpers.c",
         "i915.c",
@@ -20,7 +22,10 @@ cc_defaults {
         "virtio_gpu.c",
     ],
 
+    include_dirs: ["external/mesa3d/include"],
+
     cflags: [
+        "-DDRV_DRI_GENERIC",
         "-D_GNU_SOURCE=1",
         "-D_FILE_OFFSET_BITS=64",
         "-Wall",
@@ -32,6 +37,15 @@ cc_defaults {
     ],
 
     cppflags: ["-std=c++14"],
+
+    multilib: {
+        lib32: {
+            cflags: ["-DDRI_DRIVER_DIR=/vendor/lib/dri"],
+        },
+        lib64: {
+            cflags: ["-DDRI_DRIVER_DIR=/vendor/lib64/dri"],
+        },
+    },
 }
 
 cc_defaults {
@@ -87,28 +101,6 @@ cc_defaults {
     export_include_dirs: ["."],
 }
 
-cc_library {
-    name: "libgbm",
-    defaults: ["gbm_defaults"],
-    host_supported: true,
-
-    target: {
-        host: {
-            // Avoid linking to another host copy of libdrm; this library will cause
-            // binary GPU drivers to be loaded from the host, which might be linked
-            // to a system copy of libdrm, which conflicts with the AOSP one
-            allow_undefined_symbols: true,
-            header_libs: ["libdrm_headers"],
-        },
-        android: {
-            shared_libs: [
-                "libdrm",
-                "liblog"
-            ],
-        },
-    },
-}
-
 cc_library_static {
     name: "libminigbm_cros_gralloc",
     defaults: ["minigbm_cros_gralloc_defaults"],
@@ -122,6 +114,7 @@ cc_library_shared {
     name: "gralloc.minigbm",
     defaults: ["minigbm_cros_gralloc_defaults"],
     srcs: ["cros_gralloc/gralloc0/gralloc0.cc"],
+    relative_install_path: "hw",
 }
 
 cc_library_shared {
@@ -136,6 +129,7 @@ cc_library_shared {
             enabled: true,
         },
     },
+    relative_install_path: "hw",
     cflags: ["-DDRV_I915"],
     srcs: ["cros_gralloc/gralloc0/gralloc0.cc"],
 }
@@ -143,6 +137,7 @@ cc_library_shared {
 cc_library_shared {
     name: "gralloc.minigbm_meson",
     defaults: ["minigbm_cros_gralloc_defaults"],
+    relative_install_path: "hw",
     cflags: ["-DDRV_MESON"],
     srcs: ["cros_gralloc/gralloc0/gralloc0.cc"],
 }

cros_gralloc/cros_gralloc_driver.cc
@@ -11,6 +11,7 @@
 #include <sys/mman.h>
 #include <syscall.h>
 #include <xf86drm.h>
+#include <xf86drmMode.h>
 
 #include "../drv_priv.h"
 #include "../helpers.h"
@@ -62,14 +63,39 @@ cros_gralloc_driver::~cros_gralloc_driver()
 	}
 }
 
-static struct driver *init_try_node(int idx, char const *str)
+static bool is_kms_dev(const char *path)
+{
+	int fd = open(path, O_RDWR | O_CLOEXEC);
+	if (fd < 0)
+		return false;
+
+	auto res = drmModeGetResources(fd);
+	if (!res) {
+		close(fd);
+		return false;
+	}
+
+	bool is_kms = res->count_crtcs > 0 && res->count_connectors > 0 && res->count_encoders > 0;
+
+	drmModeFreeResources(res);
+	close(fd);
+
+	return is_kms;
+}
+
+static struct driver *init_try_node(int idx, bool use_card_node, bool try_generic)
 {
 	int fd;
 	char *node;
 	struct driver *drv;
 
-	if (asprintf(&node, str, DRM_DIR_NAME, idx) < 0)
+	if (asprintf(&node, use_card_node ? "%s/card%d" : "%s/renderD%d", DRM_DIR_NAME, idx) < 0)
 		return NULL;
+
+	if (use_card_node && !is_kms_dev(node)) {
+		free(node);
+		return NULL;
+	}
 
 	fd = open(node, O_RDWR, 0);
 	free(node);
@@ -77,7 +103,7 @@ static struct driver *init_try_node(int idx, char const *str)
 	if (fd < 0)
 		return NULL;
 
-	drv = drv_create(fd);
+	drv = drv_create(fd, try_generic);
 	if (!drv)
 		close(fd);
 
@@ -93,8 +119,6 @@ int32_t cros_gralloc_driver::init()
 	 * TODO(gsingh): Enable render nodes on udl/evdi.
 	 */
 
-	char const *render_nodes_fmt = "%s/renderD%d";
-	char const *card_nodes_fmt = "%s/card%d";
 	uint32_t num_nodes = DRM_NUM_NODES;
 	uint32_t min_render_node = DRM_RENDER_NODE_START;
 	uint32_t max_render_node = (min_render_node + num_nodes);
@@ -103,17 +127,33 @@ int32_t cros_gralloc_driver::init()
 
 	// Try render nodes...
 	for (uint32_t i = min_render_node; i < max_render_node; i++) {
-		drv_ = init_try_node(i, render_nodes_fmt);
+		drv_ = init_try_node(i, false, false);
 		if (drv_)
 			return 0;
 	}
 
 	// Try card nodes... for vkms mostly.
 	for (uint32_t i = min_card_node; i < max_card_node; i++) {
-		drv_ = init_try_node(i, card_nodes_fmt);
+		drv_ = init_try_node(i, true, false);
 		if (drv_)
 			return 0;
 	}
+
+#ifdef DRV_DRI_GENERIC
+	// Try card nodes using mesa3d/dri backend
+	for (uint32_t i = min_card_node; i < max_card_node; i++) {
+		drv_ = init_try_node(i, true, true);
+		if (drv_)
+			return 0;
+	}
+
+	// Try render nodes using mesa3d/dri backend
+	for (uint32_t i = min_render_node; i < max_render_node; i++) {
+		drv_ = init_try_node(i, false, true);
+		if (drv_)
+			return 0;
+	}
+#endif
 
 	return -ENODEV;
 }
@@ -257,7 +297,7 @@ int32_t cros_gralloc_driver::allocate(const struct cros_gralloc_buffer_descripto
 	name = (char *)(&hnd->base.data[hnd->name_offset]);
 	snprintf(name, descriptor->name.size() + 1, "%s", descriptor->name.c_str());
 
-	id = drv_bo_get_plane_handle(bo, 0).u32;
+	id = hnd->id;
 	auto buffer = new cros_gralloc_buffer(id, bo, hnd, hnd->fds[hnd->num_planes],
 					      hnd->reserved_region_size);
 
@@ -286,10 +326,7 @@ int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 		return 0;
 	}
 
-	if (drmPrimeFDToHandle(drv_get_fd(drv_), hnd->fds[0], &id)) {
-		drv_log("drmPrimeFDToHandle failed.\n");
-		return -errno;
-	}
+	id = hnd->id;
 
 	if (buffers_.count(id)) {
 		buffer = buffers_[id];
@@ -314,8 +351,6 @@ int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 		bo = drv_bo_import(drv_, &data);
 		if (!bo)
 			return -EFAULT;
-
-		id = drv_bo_get_plane_handle(bo, 0).u32;
 
 		buffer = new cros_gralloc_buffer(id, bo, nullptr, hnd->fds[hnd->num_planes],
 						 hnd->reserved_region_size);

dri.c
@@ -4,7 +4,7 @@
  * found in the LICENSE file.
  */
 
-#ifdef DRV_AMDGPU
+#if defined(DRV_DRI_GENERIC) || defined(DRV_AMDGPU)
 
 #include <assert.h>
 #include <dlfcn.h>
@@ -66,27 +66,28 @@ static bool lookup_extension(const __DRIextension *const *extensions, const char
 	return false;
 }
 
-/*
- * Close Gem Handle
- */
-static void close_gem_handle(uint32_t handle, int fd)
+int dri_bo_get_plane_fd(struct bo *bo, size_t plane)
 {
-	struct drm_gem_close gem_close = { 0 };
-	int ret = 0;
+	struct dri_driver *dri = bo->drv->priv;
+	__DRIimage *plane_image = NULL;
+	int fd = -1;
 
-	gem_close.handle = handle;
-	ret = drmIoctl(fd, DRM_IOCTL_GEM_CLOSE, &gem_close);
-	if (ret)
-		drv_log("DRM_IOCTL_GEM_CLOSE failed (handle=%x) error %d\n", handle, ret);
+	plane_image = dri->image_extension->fromPlanar(bo->priv, plane, NULL);
+	__DRIimage *image = plane_image ? plane_image : bo->priv;
+
+	dri->image_extension->queryImage(image, __DRI_IMAGE_ATTRIB_FD, &fd);
+
+	if (plane_image)
+		dri->image_extension->destroyImage(plane_image);
+
+	return fd;
 }
-
 /*
  * The DRI GEM namespace may be different from the minigbm's driver GEM namespace. We have
  * to import into minigbm.
  */
 static int import_into_minigbm(struct dri_driver *dri, struct bo *bo)
 {
-	uint32_t handle;
 	int ret, modifier_upper, modifier_lower, num_planes, i, j;
 	off_t dmabuf_sizes[DRV_MAX_PLANES];
 	__DRIimage *plane_image = NULL;
@@ -105,8 +106,6 @@ static int import_into_minigbm(struct dri_driver *dri, struct bo *bo)
 					      &num_planes)) {
 		return -errno;
 	}
-
-	bo->meta.num_planes = num_planes;
 
 	for (i = 0; i < num_planes; ++i) {
 		int prime_fd, stride, offset;
@@ -136,16 +135,14 @@ static int import_into_minigbm(struct dri_driver *dri, struct bo *bo)
 
 		lseek(prime_fd, 0, SEEK_SET);
 
-		ret = drmPrimeFDToHandle(bo->drv->fd, prime_fd, &handle);
-
 		close(prime_fd);
 
-		if (ret) {
+		if (!dri->image_extension->queryImage(image, __DRI_IMAGE_ATTRIB_HANDLE,
+						      &bo->handles[i].s32)) {
 			drv_log("drmPrimeFDToHandle failed with %s\n", strerror(errno));
+			ret = -errno;
 			goto cleanup;
 		}
-
-		bo->handles[i].u32 = handle;
 
 		bo->meta.strides[i] = stride;
 		bo->meta.offsets[i] = offset;
@@ -183,11 +180,6 @@ cleanup:
 		/* Multiple equivalent handles) */
 		if (i == j)
 			break;
-
-		/* This kind of goes horribly wrong when we already imported
-		 * the same handles earlier, as we should really reference
-		 * count handles. */
-		close_gem_handle(bo->handles[i].u32, bo->drv->fd);
 	}
 	return ret;
 }
@@ -203,7 +195,7 @@ int dri_init(struct driver *drv, const char *dri_so_path, const char *driver_suf
 
 	struct dri_driver *dri = drv->priv;
 
-	dri->fd = open(drmGetRenderDeviceNameFromFd(drv_get_fd(drv)), O_RDWR);
+	dri->fd = open(drmGetDeviceNameFromFd(drv_get_fd(drv)), O_RDWR);
 	if (dri->fd < 0)
 		return -ENODEV;
 
@@ -279,11 +271,17 @@ void dri_close(struct driver *drv)
 int dri_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32_t format,
 		  uint64_t use_flags)
 {
+	uint32_t width_ = width;
+	uint32_t height_ = height;
 	unsigned int dri_use;
 	int ret, dri_format;
+	int is_video = false;
 	struct dri_driver *dri = bo->drv->priv;
 
 	dri_format = drm_format_to_dri_format(format);
+	/* Video buffers can't be allocated using DRI */
+	if (!dri_format)
+		is_video = true;
 
 	/* Gallium drivers require shared to get the handle and stride. */
 	dri_use = __DRI_IMAGE_USE_SHARE;
@@ -294,16 +292,43 @@ int dri_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32_t forma
 	if (use_flags & BO_USE_LINEAR)
 		dri_use |= __DRI_IMAGE_USE_LINEAR;
 
-	bo->priv = dri->image_extension->createImage(dri->device, width, height, dri_format,
+	if (is_video) {
+		int stride = drv_stride_from_format(format, width, 0);
+		drv_bo_from_format(bo, stride, height, format);
+		dri_format = __DRI_IMAGE_FORMAT_R8;
+		dri_use |= __DRI_IMAGE_USE_LINEAR;
+		width_ = stride;
+		height_ = DIV_ROUND_UP(bo->meta.total_size, width_);
+	}
+
+	bo->priv = dri->image_extension->createImage(dri->device, width_, height_, dri_format,
 						     dri_use, NULL);
 	if (!bo->priv) {
 		ret = -errno;
 		return ret;
 	}
 
-	ret = import_into_minigbm(dri, bo);
-	if (ret)
-		goto free_image;
+	if (is_video) {
+		__DRIimage *plane_image = dri->image_extension->fromPlanar(bo->priv, 0, NULL);
+		__DRIimage *image = plane_image ? plane_image : bo->priv;
+
+		if (!dri->image_extension->queryImage(image, __DRI_IMAGE_ATTRIB_HANDLE,
+						      &bo->handles[0].s32)) {
+			drv_log("drmPrimeFDToHandle failed with %s\n", strerror(errno));
+			ret = -errno;
+			goto free_image;
+		}
+
+		if (plane_image)
+			dri->image_extension->destroyImage(plane_image);
+
+		for (size_t plane = 1; plane < bo->meta.num_planes; plane++)
+			bo->handles[plane].u32 = bo->handles[0].u32;
+	} else {
+		ret = import_into_minigbm(dri, bo);
+		if (ret)
+			goto free_image;
+	}
 
 	return 0;
 
@@ -323,6 +348,9 @@ int dri_bo_create_with_modifiers(struct bo *bo, uint32_t width, uint32_t height,
 	}
 
 	dri_format = drm_format_to_dri_format(format);
+
+	if (dri_format == 0)
+		return -EINVAL;
 
 	bo->priv = dri->image_extension->createImageWithModifiers(
 	    dri->device, width, height, dri_format, modifiers, modifier_count, NULL);
@@ -397,7 +425,6 @@ int dri_bo_destroy(struct bo *bo)
 	struct dri_driver *dri = bo->drv->priv;
 
 	assert(bo->priv);
-	close_gem_handle(bo->handles[0].u32, bo->drv->fd);
 	dri->image_extension->destroyImage(bo->priv);
 	bo->priv = NULL;
 	return 0;

dri.h
@@ -4,8 +4,12 @@
  * found in the LICENSE file.
  */
 
-#ifdef DRV_AMDGPU
+#if defined(DRV_DRI_GENERIC) || defined(DRV_AMDGPU)
 
+#ifndef __DRIH__
+#define __DRIH__
+
+#define HAVE_LIBDRM
 // Avoid transitively including a bunch of unnecessary headers.
 #define GL_GLEXT_LEGACY
 #include "GL/internal/dri_interface.h"
@@ -36,6 +40,10 @@ int dri_bo_import(struct bo *bo, struct drv_import_fd_data *data);
 int dri_bo_destroy(struct bo *bo);
 void *dri_bo_map(struct bo *bo, struct vma *vma, size_t plane, uint32_t map_flags);
 int dri_bo_unmap(struct bo *bo, struct vma *vma);
+int dri_bo_get_plane_fd(struct bo *bo, size_t plane);
+
 size_t dri_num_planes_from_modifier(struct driver *drv, uint32_t format, uint64_t modifier);
+
+#endif
 
 #endif

drv.c
@@ -62,6 +62,10 @@ extern const struct backend backend_virtio_gpu;
 extern const struct backend backend_udl;
 extern const struct backend backend_vkms;
 
+#ifdef DRV_DRI_GENERIC
+extern const struct backend backend_dri_generic;
+#endif
+
 static const struct backend *drv_get_backend(int fd)
 {
 	drmVersionPtr drm_version;
@@ -119,7 +123,7 @@ static const struct backend *drv_get_backend(int fd)
 	return NULL;
 }
 
-struct driver *drv_create(int fd)
+struct driver *drv_create(int fd, bool try_generic)
 {
 	struct driver *drv;
 	int ret;
@@ -135,6 +139,11 @@ struct driver *drv_create(int fd)
 
 	drv->fd = fd;
 	drv->backend = drv_get_backend(fd);
+
+#ifdef DRV_DRI_GENERIC
+	if (!drv->backend && try_generic)
+		drv->backend = &backend_dri_generic;
+#endif
 
 	if (!drv->backend)
 		goto free_driver;
@@ -606,6 +615,9 @@ int drv_bo_get_plane_fd(struct bo *bo, size_t plane)
 	if (bo->is_test_buffer) {
 		return -EINVAL;
 	}
+
+	if (bo->drv->backend->bo_get_plane_fd)
+		return bo->drv->backend->bo_get_plane_fd(bo, plane);
 
 	ret = drmPrimeHandleToFD(bo->drv->fd, bo->handles[plane].u32, DRM_CLOEXEC | DRM_RDWR, &fd);
 

drv.h
@@ -116,7 +116,7 @@ struct mapping {
 	uint32_t refcount;
 };
 
-struct driver *drv_create(int fd);
+struct driver *drv_create(int fd, bool try_generic);
 
 void drv_destroy(struct driver *drv);
 

drv_priv.h
@@ -73,6 +73,7 @@ struct backend {
 	int (*bo_compute_metadata)(struct bo *bo, uint32_t width, uint32_t height, uint32_t format,
 				   uint64_t use_flags, const uint64_t *modifiers, uint32_t count);
 	int (*bo_create_from_metadata)(struct bo *bo);
+	int (*bo_get_plane_fd)(struct bo *bo, size_t plane);
 	int (*bo_destroy)(struct bo *bo);
 	int (*bo_import)(struct bo *bo, struct drv_import_fd_data *data);
 	void *(*bo_map)(struct bo *bo, struct vma *vma, size_t plane, uint32_t map_flags);

maurossi/minigbm

commit 9f36fdb3484d4dc23e6133b3ae7d37f3bad8faf8

generic_dri: fix RGB565 drm format identifier
The correct RGB565 drm format identifier is DRM_FORMAT_RGB565

Fixes the following errors:

... E [minigbm:gralloc0.cc(161)]: Unsupported combination -- HAL format: 4, HAL usage: 288, drv_format: RG16, use_flags: 4128
... E [minigbm:gralloc0.cc(161)]: Unsupported combination -- HAL format: 4, HAL usage: 2816, drv_format: RG16, use_flags: 36

Fixes: ea2cf12a89e ("Add generic DRI backend")
Change-Id: I59f188fbcff5450f25e69d331757244c325436f3
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>

generic_dri_driver.c
@@ -31,7 +31,7 @@ static uint32_t generic_dri_resolve_format(struct driver *drv, uint32_t format, 
 
 static const uint32_t scanout_render_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_XRGB8888,
 						   DRM_FORMAT_ABGR8888, DRM_FORMAT_XBGR8888,
-						   DRM_FORMAT_BGR565 };
+						   DRM_FORMAT_RGB565 };
 
 static const uint32_t texture_only_formats[] = { DRM_FORMAT_NV12, DRM_FORMAT_NV21,
 						 DRM_FORMAT_YVU420, DRM_FORMAT_YVU420_ANDROID };